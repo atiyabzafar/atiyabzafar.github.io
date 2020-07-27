949ad1a48ea79b537bb1db1b06492b6faaa30767
atiyabzafar <63765230+atiyabzafar@users.noreply.github.com>
2020-07-27 20:04:32 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,9 +26,12 @@ svg.append("svg:defs").selectAll("marker")
 var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
-  .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(100).strength(1))
-  .force("charge", d3.forceManyBody().strength(-50))
-  .force("center", d3.forceCenter(width / 2, height / 2));
+  //.force("link", d3.forceLink().id(function(d) { return d.id; }).distance(100).strength(1))
+  //.force("charge", d3.forceManyBody().strength(-50))
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
 d3.json("data.json", function(error, graph) {
 	
